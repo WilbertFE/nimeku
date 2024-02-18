5613990a35847220e63f136bfe648acbb7257329
WilbertFE <wilbertbernadi699@gmail.com>
2024-02-18 09:29:21 +0700

    Update: top Section

---
+++ src/Layouts/Top.jsx
@@ -0,0 +1,26 @@
+import Energy from "../Fragments/Home/Energy";
+import Profile from "../Fragments/Home/Profile";
+import Premium from "../Fragments/Home/Premium";
+import Search from "../Fragments/Home/Search";
+import NavMenu from "../Fragments/Home/NavMenu";
+import LastAnime from "../Fragments/Home/LastAnime";
+import Genres from "../Fragments/Home/Genres";
+
+const Top = () => {
+  return (
+    <section id="top" className="pt-8 pb-32">
+      <div className="container">
+        <div className="flex flex-wrap">
+          <Energy />
+          <Profile />
+          <Premium />
+          <Search />
+          <NavMenu />
+          <LastAnime />
+          <Genres />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Top;
