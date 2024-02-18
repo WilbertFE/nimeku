ac0bd43f1f8bdaf706324f4628867122bea928fb
WilbertFE <wilbertbernadi699@gmail.com>
2024-02-18 17:18:24 +0700

    Update: Card UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 // import icons
 import { GrRefresh } from "react-icons/gr";
 import { FaAngleRight } from "react-icons/fa6";
-import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { PiEye } from "react-icons/pi";
 // import Layouts
 import { Fragment } from "react";
 import Top from "../Layouts/Top";
@@ -24,22 +24,55 @@ const Home = () => {
           <div className="flex flex-wrap">
             <div className="w-full flex flex-wrap">
               <div className="w-full flex items-center gap-x-2 px-6 mb-4">
-                <h1 className="text-white font-bold text-2xl">
+                <h1 className="text-white font-bold text-2xl min-w-max">
                   Episode terbaru
                 </h1>
                 <GrRefresh className="w-[32px] h-[32px] text-secondary" />
-                <h3 className="text-white font-bold ml-2 text-sm">
+                <h3 className="text-white font-bold ml-2 text-xs min-w-max">
                   Lihat Jadwal
                 </h3>
-                <FaAngleRight className="text-white w-[22px] h-[22px] relative -top-[1px]" />
+                <FaAngleRight className="text-white w-[20px] h-[20px] relative -top-[1px]" />
               </div>
-              <div className="w-full flex flex-wrap px-2">
-                <div className="w-1/3 flex flex-col">
-                  <div className="w-full flex h-[150px] bg-third animate-pulse rounded-lg mb-1"></div>
-                  <div className="flex gap-x-2 items-center">
-                    <MdOutlineRemoveRedEye className="w-[16px] h-[16px] text-white" />
-                    <p className="text-white">813</p>
+              <div className="w-full flex gap-y-3 px-1 flex-wrap">
+                <div className="w-1/3 flex flex-col px-1">
+                  <div className="w-full h-[150px] bg-third rounded-lg mb-2"></div>
+                  <div className="flex gap-x-1">
+                    <PiEye className="text-white/70 w-4 h-4 mb-1" />
+                    <p className="text-[10px] text-white/90">5,6K</p>
                   </div>
+                  <h3 className="text-white text-xs line-clamp-2">
+                    Seishun Buta Yarou wa: Odekake Sister no
+                  </h3>
+                </div>
+                <div className="w-1/3 flex flex-col px-1">
+                  <div className="w-full h-[150px] bg-third rounded-lg mb-2"></div>
+                  <div className="flex gap-x-1">
+                    <PiEye className="text-white/70 w-4 h-4 mb-1" />
+                    <p className="text-[10px] text-white/90">5,6K</p>
+                  </div>
+                  <h3 className="text-white text-xs line-clamp-2">
+                    Seishun Buta Yarou wa: Odekake Sister no
+                  </h3>
+                </div>
+                <div className="w-1/3 flex flex-col px-1">
+                  <div className="w-full h-[150px] bg-third rounded-lg mb-2"></div>
+                  <div className="flex gap-x-1">
+                    <PiEye className="text-white/70 w-4 h-4 mb-1" />
+                    <p className="text-[10px] text-white/90">5,6K</p>
+                  </div>
+                  <h3 className="text-white text-xs line-clamp-2">
+                    Seishun Buta Yarou wa: Odekake Sister no
+                  </h3>
+                </div>
+                <div className="w-1/3 flex flex-col px-1">
+                  <div className="w-full h-[150px] bg-third rounded-lg mb-2"></div>
+                  <div className="flex gap-x-1">
+                    <PiEye className="text-white/70 w-4 h-4 mb-1" />
+                    <p className="text-[10px] text-white/90">5,6K</p>
+                  </div>
+                  <h3 className="text-white text-xs line-clamp-2">
+                    Seishun Buta Yarou wa: Odekake Sister no
+                  </h3>
                 </div>
               </div>
             </div>
