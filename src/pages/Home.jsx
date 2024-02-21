4cb195ea87204389caf1cc3ee7cfa27183b21d21
WilbertFE <wilbertbernadi699@gmail.com>
2024-02-21 19:53:05 +0700

    Update UI: Sedang populer & Rekomendasi anime

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,12 @@ import { Fragment, useEffect, useState } from "react";
 import Top from "../Layouts/Top";
 
 const Home = () => {
+  // fetch new anime
   const [newAnimes, setNewAnimes] = useState(null);
   useEffect(() => {
     async function getAnimes() {
       try {
-        const url = "https://api.jikan.moe/v4/seasons/now?limit=12";
+        const url = "https://api.jikan.moe/v4/seasons/now?limit=12&sfw=true";
         const data = await fetch(url)
           .then((response) => response.json())
           .then((response) => response);
@@ -24,8 +25,42 @@ const Home = () => {
     }
     getAnimes();
   }, []);
-  console.log(newAnimes);
-
+  // fetch popular anime
+  const [popularAnime, setPopularAnime] = useState(null);
+  useEffect(() => {
+    async function getAnimes() {
+      try {
+        const url =
+          "https://api.jikan.moe/v4/top/anime?filter=airing&sfw=true&limit=25";
+        const data = await fetch(url)
+          .then((response) => response.json())
+          .then((response) => response);
+        setPopularAnime(data.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    getAnimes();
+  }, []);
+  // fetch ended anime
+  const [recAnime, setRecAnime] = useState(null);
+  useEffect(() => {
+    async function getAnimes() {
+      try {
+        const url =
+          "https://api.jikan.moe/v4/top/anime?sfw=true&filter=favorite&limit=25";
+        const data = await fetch(url)
+          .then((response) => response.json())
+          .then((response) => response);
+        setRecAnime(data.data);
+        console.log("ok");
+      } catch (error) {
+        console.error(error.message);
+        console.log("ok error");
+      }
+    }
+    getAnimes();
+  }, []);
   return (
     <Fragment>
       {
@@ -41,7 +76,7 @@ const Home = () => {
       <section id="main" className="pt-8 pb-32">
         <div className="container">
           <div className="flex flex-wrap">
-            <div className="w-full flex flex-wrap pb-12">
+            <div className="w-full flex flex-wrap pb-4">
               <div className="w-full flex items-center gap-x-2 px-6 mb-4">
                 <h1 className="text-white font-bold text-2xl min-w-max">
                   Anime Terbaru
@@ -92,7 +127,7 @@ const Home = () => {
                   ))}
               </div>
             </div>
-            <div className="w-full flex items-center px-8">
+            <div className="w-full flex items-center px-8 pb-4">
               <div className="flex flex-col bg-third rounded-lg mr-4 py-2 px-3">
                 <h1 className="text-secondary font-bold">Jadwal Rilis</h1>
                 <p className="text-white/90 text-[11px]">Tidak tahu kapan</p>
@@ -101,6 +136,100 @@ const Home = () => {
               <div>
                 <p className="text-white text-xs">Tekan di sini</p>
                 <p className="text-white text-xs">untuk melihat jadwal</p>
+              </div>
+            </div>
+            <div className="w-full flex flex-wrap px-4 pb-4">
+              <h1 className="w-full text-white font-bold text-xl mb-4">
+                Sedang Populer
+              </h1>
+              <div className="hide-scrollbar flex overflow-auto gap-x-3">
+                {popularAnime &&
+                  popularAnime.map((anime) => (
+                    <div
+                      key={anime.mal_id}
+                      className="min-w-[150px] flex flex-col"
+                    >
+                      <div className="w-full relative h-[150px] bg- rounded-md mb-2 bg-cover overflow-hidden">
+                        {/* <span className="text-primary tracking-widest absolute left-0 top-0 bg-secondary text-xs font-bold p-1 rounded-br-lg">
+                          Airing
+                        </span> */}
+                        <span className="flex absolute top-0 right-0 items-center bg-primary px-2 py-1 rounded-tr-lg">
+                          <IoStarSharp className="text-white w-3 h-3 mr-1" />
+                          <p className="text-white text-xs">{anime.score}</p>
+                        </span>
+                        <h3 className="text-secondary font-bold absolute bottom-0 left-0 text-xs bg-primary px-2 py-1 rounded-bl-lg">
+                          {anime.episodes} Eps
+                        </h3>
+                        <img
+                          src={anime.images.webp.large_image_url}
+                          alt={anime.title}
+                          loading="lazy"
+                          className="block w-full h-full object-cover text-white text-xs text-center"
+                        />
+                      </div>
+                      <div className="flex gap-x-1">
+                        <IoMdPerson className="text-white/70 w-4 h-4 mb-1" />
+                        <p className="text-[11px] text-white/90 flex-1">
+                          {Math.round(anime.scored_by / 1000)}K
+                        </p>
+                        <FaFireFlameCurved className="text-red-600 w-4 h-4" />
+                        <p className="font-bold text-white text-[12px]">Rame</p>
+                      </div>
+                      <h3 className="text-white text-sm line-clamp-2">
+                        {anime.title}
+                      </h3>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="w-full flex flex-wrap px-4">
+              <div className="w-full flex">
+                <h1 className="text-white font-bold text-xl mb-4">
+                  Rekomendasi Anime
+                </h1>
+                <div className="flex-1 flex items-center justify-end relative -top-1">
+                  <h3 className="text-white font-bold text-xs">Lihat Semua</h3>
+                  <FaAngleRight className="text-white w-[20px] h-[20px]" />
+                </div>
+              </div>
+              <div className="hide-scrollbar flex overflow-auto gap-x-3">
+                {recAnime &&
+                  recAnime.map((anime) => (
+                    <div
+                      key={anime.mal_id}
+                      className="min-w-[150px] flex flex-col"
+                    >
+                      <div className="w-full relative h-[150px] bg- rounded-md mb-2 bg-cover overflow-hidden">
+                        {/* <span className="text-primary tracking-widest absolute left-0 top-0 bg-secondary text-xs font-bold p-1 rounded-br-lg">
+                          Airing
+                        </span> */}
+                        <span className="flex absolute top-0 right-0 items-center bg-primary px-2 py-1 rounded-tr-lg">
+                          <IoStarSharp className="text-white w-3 h-3 mr-1" />
+                          <p className="text-white text-xs">{anime.score}</p>
+                        </span>
+                        <h3 className="text-secondary font-bold absolute bottom-0 left-0 text-xs bg-primary px-2 py-1 rounded-bl-lg">
+                          {anime.episodes} Eps
+                        </h3>
+                        <img
+                          src={anime.images.webp.large_image_url}
+                          alt={anime.title}
+                          loading="lazy"
+                          className="block w-full h-full object-cover text-white text-xs text-center"
+                        />
+                      </div>
+                      <div className="flex gap-x-1">
+                        <IoMdPerson className="text-white/70 w-4 h-4 mb-1" />
+                        <p className="text-[11px] text-white/90 flex-1">
+                          {Math.round(anime.scored_by / 1000)}K
+                        </p>
+                        <FaFireFlameCurved className="text-red-600 w-4 h-4" />
+                        <p className="font-bold text-white text-[12px]">Rame</p>
+                      </div>
+                      <h3 className="text-white text-sm line-clamp-2">
+                        {anime.title}
+                      </h3>
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
