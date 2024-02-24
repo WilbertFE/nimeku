3147fe6e3efebcce02ab88fa7001ca8e14b97617
WilbertFE <wilbertbernadi699@gmail.com>
2024-02-24 15:02:38 +0700

    Done: Register and login

--- src/Fragments/Home/Top/Profile.jsx
+++ src/Fragments/Home/Top/Profile.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import { IoMdSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
 
+const defaultData = {
+  name: "Anda belum login",
+  level: 1,
+};
+
 const Profile = () => {
+  const [account, setAccount] = useState(defaultData);
+
+  useEffect(() => {
+    if (localStorage.getItem("nimekuAccount")) {
+      const accountData = JSON.parse(localStorage.getItem("nimekuAccount"));
+      setAccount(accountData);
+    }
+  }, []);
+
   return (
     <div className="w-full flex items-center mb-6 gap-x-3 px-4">
       <div className="w-[56px] h-[56px] rounded-full bg-black overflow-hidden border border-white">
@@ -9,11 +24,21 @@ const Profile = () => {
       </div>
       <div className="flex flex-col flex-1 overflow-hidden">
         <span className="text-white/80 text-sm">Welcome,</span>
-        <h1 className="text-blue-600 underline truncate tracking-widest">
-          <Link to="/login">Anda belum login</Link>
+        <h1
+          className={`truncate tracking-widest ${
+            account.name === "Anda belum login"
+              ? "text-blue-500 underline"
+              : "text-white"
+          }`}
+        >
+          {account.name === "Anda belum login" ? (
+            <Link to="/login">{account.name}</Link>
+          ) : (
+            account.name
+          )}
         </h1>
         <div className="flex items-center">
-          <p className="text-white mr-2 tracking-wide">Lvl. 1</p>
+          <p className="text-white mr-2 tracking-wide">Lvl. {account.level}</p>
           <span className="block flex-1 h-[5px] bg-fourth"></span>
           <p className="text-white ml-2">0%</p>
         </div>
