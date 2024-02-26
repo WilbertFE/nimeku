1dd17ed53c1be19673ae07cb1586ab72975d372f
WilbertFE <wilbertbernadi699@gmail.com>
2024-02-26 19:05:07 +0700

    Update: search Page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,36 +1,75 @@
 import { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import AnimeCard from "../Fragments/AnimeCard";
+
 const SearchPage = () => {
-  const [searchAnime, setSearchAnime] = useState(null);
-  const [value, setValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [animes, setAnimes] = useState(null);
+  const [isLoad, setIsLoad] = useState(false);
 
   useEffect(() => {
-    const fetchSearch = async () => {
+    // guard
+    if (searchValue === "") {
+      setAnimes(null);
+      setIsLoad(false);
+      return;
+    }
+
+    // loading
+    setIsLoad(true);
+
+    const getData = async () => {
       try {
-        const url = ``;
-      } catch (error) {
-        console.log(error.message);
+        const url = `https://api.jikan.moe/v4/anime?q=${searchValue}&sfw&unapproved&limit=25&order_by=popularity&rating=pg13`;
+        const data = await fetch(url).then((response) => {
+          if (response.status !== 200) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        });
+        setIsLoad(false);
+        setAnimes(data.data);
+      } catch (err) {
+        console.error(err.message);
       }
     };
-    fetchSearch();
-  }, [value]);
+    getData();
+  }, [searchValue]);
 
   return (
-    <div className="container bg-primary min-h-screen">
-      <div className="flex flex-wrap mx-4">
-        <div className="w-full flex flex-col mt-6">
+    <div className="container p-4 bg-primary min-h-screen">
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-y-6 mb-8">
           <Link to="/">
-            <FaArrowLeft className="text-white text-xl mb-6" />
+            <FaArrowLeft className="text-white text-xl" />
           </Link>
           <input
             type="text"
-            name="search-anime"
-            id="search-anime"
+            name="anime-search"
+            id="anime-search"
             placeholder="Tulis anime yang mau dicari"
-            className="w-full h-[50px] bg-third focus:outline-none p-4 text-white rounded-lg"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="block bg-third h-[50px] rounded-lg px-3 focus:outline-none text-white"
           />
         </div>
+        {animes && !isLoad ? (
+          <div className="flex flex-wrap gap-y-4">
+            {animes.map((anime, i) => (
+              <div key={i} className="w-1/3 flex flex-col px-1">
+                <AnimeCard>
+                  <AnimeCard.Header anime={anime} isNew={false} />
+                  <AnimeCard.Body anime={anime} isPopular={false} />
+                  <AnimeCard.Footer anime={anime} />
+                </AnimeCard>
+              </div>
+            ))}
+          </div>
+        ) : null}
+        {isLoad && (
+          <div className="w-[50px] h-[50px] self-center mt-32 bg-transparent border border-white border-l-transparent border-t-transparent rounded-full animate-spin"></div>
+        )}
       </div>
     </div>
   );
